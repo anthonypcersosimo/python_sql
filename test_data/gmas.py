ec1104ae411dfbe902b619a2d1dfbe66073da4e2
Anthony <anthony.cersosimo@gmail.com>
2020-01-22 11:58:01 -0500

    clean up repository and create the general template for grabbing and manipulating data, the GMAS

--- test_data/gmas.py
+++ test_data/gmas.py
@@ -1,3 +1,5 @@
+# # GMAS STANDS FOR GRAB-MANIPULATE-AND-SAVE !!
+
 import sqlite3
 import pandas as pd
 import mysql.connector
@@ -7,6 +9,7 @@ f = open("secret.txt", "r")
 if f.mode == "r":
     secret = f.read()
 
+# Use table name
 db_name = 'ecs'
 mydb = mysql.connector.connect(
     host='127.0.0.1',
@@ -21,16 +24,20 @@ print(mydb)
 # Create a cursor (an instance) to manipulate your SQL Database
 mycursor = mydb.cursor(buffered=True)
 
-# # SELECT ONLY CERTAIN FIELDS ===========================
-
+# Declare table name and query
 table_name = 'ec_universe_20191231'
-mycursor.execute(f'SELECT * FROM {table_name}')
+query = f'SELECT * FROM {table_name}'
+
+# Pass query into the cursor to execute and fetch the result
+mycursor.execute(query)
 
 result = mycursor.fetchall()
 
+# Create data frame from resul
 df = pd.DataFrame.from_records(
     result,
     columns=[i[0] for i in mycursor.description]
 )
 
-df.to_csv(r"C:\Users\Anthony's Razer\Desktop\Repositories\python_sql\csv\ec_universe_20190531_retail.csv")
+# Save dataframe to CSV format
+# df.to_csv(r"C:\Users\Anthony's Razer\Desktop\Repositories\python_sql\csv\example.csv")
---
+++ test_data/queries/masterQuery.py
@@ -0,0 +1,14 @@
+# # Get Data Flags
+# query = f'SELECT last_known_ticker, data_flag from {table_name} WHERE data_flag = "Y"'
+# df.to_csv(r"C:\Users\Anthony's Razer\Desktop\Repositories\python_sql\csv\data_flags.csv")
+
+# # Create Retail Universe
+# query = f'SELECT * from {table_name}'
+# df.to_csv(r"C:\Users\Anthony's Razer\Desktop\Repositories\python_sql\csv\ec_universe_20190531_retail.csv")
+
+# # Find Chars in Column Data
+# query = f'SELECT * FROM {table_name} WHERE field_name LIKE "%Example%"'
+
+# # Group By
+# df['mvd_sector_rank'] = df.groupby('gics_sector')['mean_valuation_deviation'].rank(ascending=False)
+
