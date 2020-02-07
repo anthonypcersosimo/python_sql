e3bb619bb14136dbd33f7e71b2d66b01bc6f958b
Anthony <anthony.cersosimo@gmail.com>
2020-02-07 08:43:06 -0500

    changed groupby and rankby scripts to be much more straight forward, stored stock data in script so you can see what data I am mutating.

--- test_data/rankBy.py
+++ test_data/rankBy.py
@@ -1,62 +1,14 @@
-import sqlite3
 import pandas as pd
-import mysql.connector
-from sqlalchemy import create_engine
 
-f = open('secret.txt', 'r')
-if f.mode == 'r':
-    secret = f.read()
+data = [['AAPL', "Consumer Discretionary", 12.88], ['TSLA', "Consumer Discretionary", 92.52], [
+    'NVDA', "Information Technology", 221.37], ['CSCO', "Information Technology", 631.47]]
 
-db_name = 'stocks'
-table_name = 'russell3000'
-
-mydb = mysql.connector.connect(
-    host='127.0.0.1',
-    port='3306',
-    db=db_name,
-    use_unicode='true',
-    username='root',
-    passwd=secret
-)
-
-if mydb:
-    print(f'Connected to SQL Database: {db_name}')
-
-# Create a cursor (an instance) to manipulate your SQL Database
-mycursor = mydb.cursor(buffered=True)
-
-# Rank Sector by Price ===============
-
-# Start by building your query, then executing it and storing the result in a variable
-query = f'SELECT * FROM {table_name}'
-mycursor.execute(query)
-
-result = mycursor.fetchall()
-# print(result, '==============================')
-
-# Convert MySQL data into DataFrame WITH headers
-df = pd.DataFrame.from_records(
-    result, columns=[i[0] for i in mycursor.description])
-# print(df)
+df = pd.DataFrame(data, columns=['ticker', 'sector', 'price'])
 
 # Create a column called Price_Rank and rank the price descending by Sector
-df['Price_Rank'] = df.groupby('Sector')['Price'].rank(ascending=False)
-
-# Create a column called Price_Pct_Rank and rank tge price descending by Sector in percentile rank
-
+df['price_rank'] = df.groupby('sector')['price'].rank(ascending=False)
 
 # Print your shiny new data frame sorted by Sector A-Z and then Price_Rank Hi-Lo
-sortedDf = df.sort_values(['Sector', 'Price_Rank'], ascending=[False, True])
+sortedDf = df.sort_values(['sector', 'price_rank'], ascending=[False, True])
 
 print(sortedDf)
-
-# # Attempt to store modified dataFrame back into SQL table
-# engine = create_engine("mysql://root:curb3436ale7770dog!@localhost/stocks")
-# sortedDf.to_sql(name='russell3000', con=engine, if_exists='replace')
-
-# # Start by building your query, then executing it and storing the result in a variable
-# queryCheck = f'SELECT * FROM {table_name}'
-# mycursor.execute(queryCheck)
-
-# check_result = mycursor.fetchall()
-# print(check_result)
